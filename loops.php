5df94e6e7d246b2987467a44d0d30753b17bb035
Srinath <124909811+srinathHD@users.noreply.github.com>
2023-02-23 15:34:02 +0530

    Update loops.php

--- loops.php
+++ loops.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>23rd feb Task</title>
     <style>
         div {
             border: 1px solid blueviolet;
